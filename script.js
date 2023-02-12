2fa3fc59feb015de269c45abd399e973c08f2f6e
Kelvin Enaro <kelvinenaro@Kelvins-MacBook-Pro.local>
2023-02-12 08:13:22 +0000

    fixed API Key but future forecast needs work

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ setInterval(() => {
 
     timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes+  ' ' + `<span id="am-pm">${ampm}</span>`
     dateEl.innerHTML = days[day] + ', ' + date+  ' ' + months[month]
-
 }, 1000);
 
 getWeatherData()
@@ -31,10 +30,8 @@ function getWeatherData() {
     navigator.geolocation.getCurrentPosition((success) => {
         console.log(success);
 
-        let {latitude, longitude} = success.coords;
-
-        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
-        .then(response => response.json())
+        fetch ("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London?unitGroup=uk&key=MY7QQT78G5GFNHASXZ7FUYXJX&contentType=json")
+        .then(res => res.json())
         .then(data =>{
             console.log(data)
             showWeatherData(data);
@@ -44,7 +41,61 @@ function getWeatherData() {
     })
     
 }
-function showWeatherData(data) {
+moment().format("dddd, MMMM Do, h:mm:ss a");
 
+function showWeatherData(data) {
+    let {temp, humidity, pressure, sunrise, sunset, windspeed, feelslike, conditions} = data.currentConditions;
+
+    currentWeatherItemsEl.innerHTML = 
+  
+` 
+<div class="weather-item">
+    <div>Temperature</div>
+    <div>${temp} &#176;C</div>
+</div>
+<div class="weather-item">
+    <div>Feels Like</div>
+    <div>${feelslike} &#176;C</div>
+</div>
+    <div class="weather-item">
+    <div>Humidity</div>
+    <div>${humidity}%</div>
+</div>
+<div class="weather-item">
+    <div>Pressure</div>
+    <div>${pressure}</div>
+</div>
+<div class="weather-item">
+    <div>Wind Speed</div>
+    <div>${windspeed}</div>
+</div>
+
+<div class="weather-item">
+    <div>Conditions</div>
+    <div>${conditions}</div>
+</div>
+
+
+
+
+`;
+
+let otherDayForecast = ''
+data.days.forEach((day, idx) => {
+    if(idx == 0){
+
+    }else{
+        otherDayForecast += `
+        <div class="weather-forecast-item"> 
+        <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+        <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="weather-icon" class="w-icon">    
+        <div class="temp">Day 12.8- &#176; C</div>
+        <div class="temp">Night 7.4- &#176; C</div>
+    </div>
+    `
+    }
+})
     
 }
+
+weatherForecastEl.innerHTML = otherDayForecast;
